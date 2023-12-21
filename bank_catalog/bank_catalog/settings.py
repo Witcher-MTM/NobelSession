b11c96bed27da17bb24661d4d3ac232d02ec9d0c
Witcher-MTM <phantomit228@gmail.com>
2023-12-21 11:17:58 +0200

    Update settings.py

--- bank_catalog/bank_catalog/settings.py
+++ bank_catalog/bank_catalog/settings.py
@@ -24,7 +24,7 @@ MEDIA_URL = '/media/'
 SECRET_KEY = 'django-insecure-iq1q%#bbu9*(*#u$vwbrbs7#+&2z%#&p6b@nr7ip_4%6xs%e17'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
